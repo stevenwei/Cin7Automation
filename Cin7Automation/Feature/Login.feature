83882ddf3ce537411ec769930c9b2a308c2785f6
Steven Wei <stevenwei89@gmail.com>
2021-06-13 18:24:36 +1200

    Add project files.

---
+++ Cin7Automation/Feature/Login.feature
@@ -0,0 +1,24 @@
+﻿Feature: Login
+
+As a customer
+I want to login to cin7 website
+So that I can access my account information
+
+Acceptance Criteria
+* User entering correct password can login
+* User entering wrong password cannot login
+
+@Regressiontest
+@Browser: Chrome
+Scenario Outline:  Verify login functionality
+
+Given I visit Cin7 website
+And I go to login page
+When I enter my <username> and <password>
+And I click login button
+Then I can successfully login
+
+Examples: 
+| username | password |
+| abc      | 123      |
+| test     | test     |
---
+++ Cin7Automation/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Cin7Automation")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Cin7Automation")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9d7007fe-2a94-4477-824d-ca2c91f77159")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
